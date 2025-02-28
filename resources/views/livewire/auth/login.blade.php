1400fba720806619e048417950a3770ee2807367
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-02-28 14:53:47 +0330

    Add Persian datepicker component with Livewire integration
    Introduced a new Persian datepicker component leveraging the "alirahimi/livewire-persian-datepicker" library. This includes JavaScript functionality, Blade component integration, and dependencies such as Jalali Moment and Alpine.js. Adjusted package configurations and composer dependencies accordingly.

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -90,7 +90,26 @@ new #[Layout('components.layouts.auth')] class extends Component {
             autocomplete="email"
             placeholder="email@example.com"
         />
+        <flux:modal.trigger name="edit-profile">
+            <flux:button>Edit profile</flux:button>
+        </flux:modal.trigger>
 
+        <flux:modal name="edit-profile" variant="flyout" class="space-y-6">
+            <div>
+                <flux:heading size="lg">Update profile</flux:heading>
+                <flux:subheading>Make changes to your personal details.</flux:subheading>
+            </div>
+
+            <flux:input label="Name" placeholder="Your name" />
+
+            <flux:input label="Date of birth" type="datetime-local" />
+
+            <div class="flex">
+                <flux:spacer />
+
+                <flux:button type="submit" variant="primary">Save changes</flux:button>
+            </div>
+        </flux:modal>
         <!-- Password -->
         <div class="relative">
             <flux:input
